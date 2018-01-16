3dcd9929ad4d5380a021fa61b87ce7fc12ffd044
Doolly <treeland88@hotmail.com>
2018-01-17 03:42:43 +0900

    구동까진 본코드
    ㅋㅋ

--- MOTOR_functions.ino
+++ MOTOR_functions.ino
@@ -2,18 +2,19 @@ void MotorInit() {
   for (int i = 0; i < 5; i++) {
     pinMode(motor_en_array[i], OUTPUT);
     pinMode(motor_dir_array[i], OUTPUT);
-    digitalWrite(motor_en_array[i], LOW);
+    digitalWrite(motor_en_array[i], HIGH);
     digitalWrite(motor_dir_array[i], LOW);
+    delay(1000);
   }
 }
 
 
 
-int last_pos = 0;
+int last_pos[5] = {0,0,0,0,0};
 void MotorCtrl (int moter_num, int range, int pos_in) {
   pos_in = constrain(pos_in, 0, range);
 
-  int pos_to_move = pos_in - last_pos;
+  int pos_to_move = pos_in - last_pos[moter_num];
 
   if (pos_to_move > 0) {
     digitalWrite(motor_en_array[moter_num], HIGH);
@@ -24,13 +25,13 @@ void MotorCtrl (int moter_num, int range, int pos_in) {
   else if (pos_to_move < 0) {
     digitalWrite(motor_en_array[moter_num], HIGH);
     digitalWrite(motor_dir_array[moter_num], CONTRACT);
-    MillisDelay(abs(pos_to_move));
+   MillisDelay(abs(pos_to_move));
     digitalWrite(motor_en_array[moter_num], LOW);
   }
   else {
     digitalWrite(motor_en_array[moter_num], LOW);
   }
 
-  last_pos = last_pos + pos_to_move;
+  last_pos[moter_num] = last_pos[moter_num] + pos_to_move;
 }
 
